82309476ef8f3bd4bd9abdc8784b87c610ac5bb1
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2022-05-12 08:52:43 +1000

    add apex charts library

--- client/src/components/BudgetSummary.js
+++ client/src/components/BudgetSummary.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts'
 
-const BudgetSummary = () => {
-  return (
-    <div>BudgetSummary</div>
-  )
+class BudgetSummary extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {},
+      series: [44, 55, 41, 17, 15],
+      labels: ['A', 'B', 'C', 'D', 'E']
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="donut">
+        <Chart options={this.state.options} series={this.state.series} type="donut" width="380" />
+      </div>
+    );
+  }
 }
 
-export default BudgetSummary
+export default BudgetSummary;
