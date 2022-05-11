df5a771acef40630dda2361f24f3a942b001eb34
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2022-05-12 08:12:40 +1000

    add income form layout and elements

--- client/src/components/IncomeForm.js
+++ client/src/components/IncomeForm.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Form, Button } from 'react-bootstrap';
 
 const IncomeForm = () => {
-  return (
-    <div>IncomeForm</div>
-  )
+    return (
+
+        <Form>
+            <Form.Group className="mb-3" controlId="formIncomeName">
+                <Form.Control type="text" placeholder="Enter Income Name" />
+                <Form.Text className="text-muted">
+                    Enter the name of Income, e.g. Salary, Bonus, etc.
+                </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formIncomeAmount">
+                <Form.Control type="number" placeholder="Amount" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Add Income
+            </Button>
+        </Form>
+
+    )
 }
 
 export default IncomeForm
