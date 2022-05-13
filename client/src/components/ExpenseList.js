aa6f069de1b37633b6253bb5273b5815c5948b26
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2022-05-14 09:53:51 +1000

    add income and expense list components

--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import { ListGroup, Badge } from 'react-bootstrap';
 
 const ExpenseList = () => {
-  return (
-    <div>ExpenseList</div>
-  )
+    return (
+        <>
+            <p>Expenses</p>
+            <ListGroup>
+                <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Fuel</div>
+                        
+                    </div>
+                    <Badge bg="primary" pill>
+                        $100
+                    </Badge>
+                </ListGroup.Item>
+                <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Shopping</div>
+                        
+                    </div>
+                    <Badge bg="primary" pill>
+                        $300
+                    </Badge>
+                </ListGroup.Item>
+                <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Back To School</div>
+                        
+                    </div>
+                    <Badge bg="primary" pill>
+                        $500
+                    </Badge>
+                </ListGroup.Item>
+              
+            </ListGroup>
+        </>
+
+
+    )
 }
 
 export default ExpenseList
--- client/src/components/IncomeList.js
+++ client/src/components/IncomeList.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { ListGroup, Badge } from 'react-bootstrap';
 
 const IncomeList = () => {
-  return (
-    <div>IncomeList</div>
-  )
+    return (
+        <>
+            <p>Income</p>
+            <ListGroup>
+                <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Wage (Monthly)</div>
+                        
+                    </div>
+                    <Badge bg="primary" pill>
+                        $2000
+                    </Badge>
+                </ListGroup.Item>
+                <ListGroup.Item
+                    as="li"
+                    className="d-flex justify-content-between align-items-start"
+                >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Freelancing</div>
+                        
+                    </div>
+                    <Badge bg="primary" pill>
+                        $500
+                    </Badge>
+                </ListGroup.Item>
+                
+              
+            </ListGroup>
+        </>
+
+
+    )
 }
 
 export default IncomeList
