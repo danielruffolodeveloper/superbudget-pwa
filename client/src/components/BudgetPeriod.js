d579a91da82bc3f45a86b0e6c3503a64d51e51e2
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2022-05-16 08:32:44 +1000

    income and expenses markers

--- client/src/components/BudgetPeriod.js
+++ client/src/components/BudgetPeriod.js
@@ -1,21 +1,36 @@
-import { Button, ListGroup } from "react-bootstrap"
+import { Button, ListGroup, Badge } from "react-bootstrap"
 import { useRef } from "react"
 import { useBudgets } from "../contexts/BudgetsContext"
 
 const BudgetPeriod = () => {
     const { handleSeedBudgets, handleGetBudgets, handleGetSelectedBudget, selectedBudget } = useBudgets()
     const budgets = handleGetBudgets()
+
+    const formatCurrency = (amount) => {
+        return amount.toLocaleString('en-AU', { style: 'currency', currency: 'AUD' });
+    }
+
     return (
         <>
             <ListGroup>
                 {budgets.map(budget => (
                     <ListGroup.Item key={budget.id}
-                        as="li" active={selectedBudget.id === budget.id}
+                        as="li"
+                        active={selectedBudget.id === budget.id}
                         onClick={() => {
                             handleGetSelectedBudget(budget.id)
                         }}
-                    >
-                        {budget.month}
+                        className="d-flex justify-content-between align-items-start">
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">{budget.month}</div>
+                        </div>
+                        <Badge bg="light" text="dark" pill>
+                            {formatCurrency(budget.incomes.reduce((acc, curr) => {
+                                return acc + parseInt(curr.amount)
+                            }, 0) - budget.expenses.reduce((acc, curr) => {
+                                return acc + parseInt(curr.amount)
+                            }, 0))}
+                        </Badge>
                     </ListGroup.Item>
                 ))}
             </ListGroup>
