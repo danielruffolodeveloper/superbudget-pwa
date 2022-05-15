b541149e14bb54966727a796d2ef80f762bd2589
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2022-05-15 14:17:30 +1000

    function to generate random budget for all 12 months

--- client/src/components/BudgetPeriod.js
+++ client/src/components/BudgetPeriod.js
@@ -4,51 +4,19 @@ import { v4 as uuidV4 } from "uuid"
 import { useRef } from "react"
 import { useBudgets } from "../contexts/BudgetsContext"
 
-const BudgetPeriod = ({ budgetMonth, setBudgetMonth }) => {
+const BudgetPeriod = () => {
 
     const budgetMonthRef = useRef()
-    const { createBudget } = useBudgets()
+    const { handleSeedBudgets } = useBudgets()
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        createBudget({
-            id: uuidV4(),
-            budgetDate: Date.now(),
-            budgetMonth: budgetMonthRef.current.value,
-            budgetIncomes: [],
-            budgetExpenses: [],
-            netIncomes: 0,
-            netExpenses: 0,
-            netBudget: 0,
-            totalSaved: 0
-        })
-    }
+
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBudgetMonth">
-                <Form.Control as="select" ref={budgetMonthRef}
-                    onChange={handleSubmit}
-                >
-                    <option>January</option>
-                    <option>February</option>
-                    <option>March</option>
-                    <option>April</option>
-                    <option>May</option>
-                    <option>June</option>
-                    <option>July</option>
-                    <option>August</option>
-                    <option>September</option>
-                    <option>October</option>
-                    <option>November</option>
-                    <option>December</option>
-                </Form.Control>
-                <Form.Text className="text-muted">
-                    Set Budget Month
-                </Form.Text>
-            </Form.Group>
-
-        </Form>
+        <>
+            <Button variant="primary" onClick={() => handleSeedBudgets()}>
+                Seed Budgets
+            </Button>
+        </>
     )
 }
 
--- client/src/contexts/BudgetsContext.js
+++ client/src/contexts/BudgetsContext.js
@@ -6,31 +6,82 @@ const BudgetsContext = React.createContext()
 
 
 export function useBudgets() {
-  return useContext(BudgetsContext)
+    return useContext(BudgetsContext)
 }
 
 export const BudgetsProvider = ({ children }) => {
-  const [budgets, setBudgets] = useLocalStorage("budgets", [])
-  const [selectedBudget, setSelectedBudget] = useLocalStorage("selectedBudget", [])
+    const [budgets, setBudgets] = useLocalStorage("budgets", [])
+    const [selectedBudget, setSelectedBudget] = useLocalStorage("selectedBudget", [])
 
-  const createBudget = (budget) => {
-    setBudgets([...budgets, { ...budget}])
+    const handleSeedBudgets = () => {
+        // delete all local storage
+        localStorage.clear()
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        const years = [2020]
+        const incomeTypes = ["Wage","Bonus","Freelancing","Other"]
+        const expenseTypes = ["Rent","Shopping","Food","Transport","Utilities","Insurance","Health","Clothing","Entertainment","Debt","Other"]
+        const budgetMonths = []
+        
+        years.forEach(year => {
+            months.forEach(month => {
+                const budgetMonth = {
+                    id: uuidV4(),
+                    month: month,
+                    year: year,
+                    incomes: [
+                        {
+                            id: uuidV4(),
+                            incomeType: incomeTypes[Math.floor(Math.random() * incomeTypes.length)],
+                            amount: Math.floor(Math.random() * (5000 - 500 + 1)) + 500
+                        },
+                        {
+                            id: uuidV4(),
+                            incomeType: incomeTypes[Math.floor(Math.random() * incomeTypes.length)],
+                            amount: Math.floor(Math.random() * (5000 - 500 + 1)) + 500
+                        }
+                    ],
+                    expenses: [
+                        {
+                            id: uuidV4(),
+                            expenseType: expenseTypes[Math.floor(Math.random() * expenseTypes.length)],
+                            amount: Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+                        },
+                        {
+                            id: uuidV4(),
+                            expenseType: expenseTypes[Math.floor(Math.random() * expenseTypes.length)],
+                            amount: Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+                        },
+                        {
+                            id: uuidV4(),
+                            expenseType: expenseTypes[Math.floor(Math.random() * expenseTypes.length)],
+                            amount: Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+                        },
+                        {
+                            id: uuidV4(),
+                            expenseType: expenseTypes[Math.floor(Math.random() * expenseTypes.length)],
+                            amount: Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+                        },
+                    ],
+                }
+                budgetMonths.push(budgetMonth)
+            })
+        })
+        setBudgets(budgetMonths)
     }
 
 
+    return (
+        <BudgetsContext.Provider
+            value={{
+                budgets,
+                setBudgets,
+                selectedBudget,
+                setSelectedBudget,
+                handleSeedBudgets
 
-  return (
-    <BudgetsContext.Provider
-      value={{
-        budgets,
-        setBudgets,
-        selectedBudget,
-        setSelectedBudget,
-        createBudget
-
-      }}
-    >
-      {children}
-    </BudgetsContext.Provider>
-  )
+            }}
+        >
+            {children}
+        </BudgetsContext.Provider>
+    )
 }
