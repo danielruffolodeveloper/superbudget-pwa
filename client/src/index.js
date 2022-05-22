ee8766c488af283017df94f3e8175b86d9cbd1d3
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2022-05-22 13:49:23 +1000

    add serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BudgetsProvider } from "./contexts/BudgetsContext"
+import * as serviceWorker from './serviceWorker';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,3 +20,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
+
