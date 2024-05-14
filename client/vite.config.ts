ba52130ec82912468a01cf4fddac85b455f8d806
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2024-05-15 00:24:03 +1000

    configured eslint

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5600,
+    strictPort: true,
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    port: 5600,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5600",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
